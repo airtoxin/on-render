c36e167e48f861d96f5de0451c9aa699a977a684
airtoxin <airtoxin@icloud.com>
2015-09-17 23:31:28 +0900

    No babel

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = {
 	create: function (listenerFunc) {
 		return function (req, res, next) {
 			var prevRender = req.app.render;
-			req.app.render = function ( view, options, callback ) {
-				var newCallback = function ( error, str ) {
+			req.app.render = function (view, options, callback) {
+				var newCallback = function (err, str) {
 					listenerFunc(req, res, str, view, options);
-					callback.apply( this, arguments );
+					callback.apply(this, arguments);
 				};
-				prevRender.call( this, view, options, newCallback );
+				prevRender.call(this, view, options, newCallback);
 			};
 			next();
 		};
