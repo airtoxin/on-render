0f572d6867599fca0e1d894f88573d129132eb90
airtoxin <airtoxin@icloud.com>
2015-09-17 15:01:58 +0900

    sketch

---
+++ src/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+	create: function (listenerFunc) {
+		return function (req, res, next) {
+			var prevRender = req.app.render;
+			req.app.render = function ( view, options, callback ) {
+				var newCallback = function ( error, str ) {
+					listenerFunc(req, res, str, view, options);
+					callback.apply( this, arguments );
+				};
+				prevRender.call( this, view, options, newCallback );
+			};
+			next();
+		};
+	}
+};
