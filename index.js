7284b2153a2d7ee3cdb0a6d8319f9fb5d077ebb8
airtoxin <airtoxin@icloud.com>
2015-09-25 16:15:18 +0900

    Mark function injected

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = {
 	create: function (listenerFunc) {
 		return function (req, res, next) {
 			var prevRender = req.app.render;
+			if (req.app.__on_render_injected__) return next();
+			req.app.__on_render_injected__ = 1;
 			req.app.render = function (view, options, callback) {
 				var newCallback = function (err, str) {
 					listenerFunc(req, res, str, view, options);
